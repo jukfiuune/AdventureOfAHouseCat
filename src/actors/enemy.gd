923b93d210c65a8842124fc1c81ac613b8a617d6
BozhidarLazarov <bozhidar.lazarov.123@gmail.com>
2023-03-10 13:05:02 +0200

    level 2

--- src/actors/enemy.gd
+++ src/actors/enemy.gd
@@ -7,8 +7,6 @@ export var gravity: = 4000.0
 
 var _velocity: = Vector2.ZERO
 
-onready var sprite = $Sprite
-
 
 func _ready() -> void :
 	_velocity.x = -speed.x
@@ -17,5 +15,4 @@ func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
 	if is_on_wall():
 		_velocity.x *= -1.0
-		sprite.scale.x *= -1
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
