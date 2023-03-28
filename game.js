3d2edf41dba7d8cf0d9f192393da930355e5a332
jukfiuune <jukfiuune@gmail.com>
2023-03-28 16:38:44 +0300

    Basic movement

--- game.js
+++ game.js
@@ -1,69 +1,89 @@
-/////////////////////
-/////PISHTOV 2.0/////
-/////////////////////
-
-
-///////////////////
-/////FUNCTIONS/////
-///////////////////
-
-// distance(x1, y1, x2, y2);                    - distance between two points
-
-// getTriangleArea(x1, y1, x2, y2, x3, y3);     - area of a triangle
-
-// isInTriangle(pX, pY, x1, y1, x2, y2, x3, y3);- is P in triangle
-
-// isInHexagon(cX, cY, strana, pX, pY);         - is P in hexagon
-
-// traceHexagonPath(cX, cY, strana);            - fill the path of a hexagon
-// Use with context.fill() or context.stroke();
-
-// pauseUpdate();                               - pauses the update function
-// Use ONLY IN update/keyup/mouseup/draw() { .. }
-
-// startUpdate();                               - start the update function
-// Use ONLY IN update/keyup/mouseup/draw() { .. }
-
-// isUpdatePaused();                            - is the update paused or not
-
-// setCanvasSize(width, height);                - change size of canvas
-
-// setFullscreen();                             - set the canvas to fullscreen
-
-
-
-///////////////////
-/////TEST GAME/////
-///////////////////
-
 setCanvasSize(800, 600);
 
+let myX = 300,
+    myY = 300,
+    myW = 50,
+    myH = 60,
+    myDX = 0,
+    myDY = 0;
+
+let wX = 200,
+    wY = 400,
+    wW = 200,
+    wH = 100;
+
+let maxSpeed = 5, acceleration = 0.5, friction = 1.4, gravity = 0.05;
+
+let isOnFloor = false;
+
 function update() {
-    console.log("Update");
+    if(myDX >= -maxSpeed && myDX <= maxSpeed) {
+        if(isKeyPressed[key_left] || isKeyPressed[key_a]) {
+            myDX -= acceleration;
+        } else if(isKeyPressed[key_right] || isKeyPressed[key_d]) {
+            myDX += acceleration;
+        } else {
+            myDX/=friction;
+        }
+    } else {
+        if(myDX < -maxSpeed) {
+            myDX = -maxSpeed;
+        } else if(myDX > maxSpeed) {
+            myDX = maxSpeed;
+        }
+    }
+    myDY += gravity;    
+    myX += myDX;
+    myY += myDY;
+    if(areColliding(myX + 5, myY + myH, myW - 10, 1, wX, wY, wW, wH)) {
+        myY -= myDY;
+        myDY = 0;
+        console.log("bottom");
+        isOnFloor = true
+    }else{
+        isOnFloor = false
+    }
+    if(areColliding(myX + 5, myY, myW - 10, 1, wX, wY, wW, wH)) {
+        myY -= myDY;
+        myDY = 0;
+        console.log("top");
+    }
+    
+    if(areColliding(myX, myY + 5, 1, myH - 10, wX, wY, wW, wH)) {
+        myX -= myDX;
+        myDX = 0;
+        console.log("left");
+    }
+    if(areColliding(myX + myW, myY + 5, 1, myH - 10, wX, wY, wW, wH)) {
+        myX -= myDX;
+        myDX = 0;
+        console.log("right");
+    }
 }
 function draw() {
-    // Draw mouse cursor
-    context.beginPath();
-    context.arc(mouseX, mouseY, 20, 0, 2*Math.PI);
-    context.fill();
+
+    context.fillStyle = "blue";
+    context.fillRect(myX, myY, myW, myH);
+    context.fillRect(wX, wY, wW, wH);
+
+    context.fillStyle = "green";
+    context.fillRect(myX + 5, myY + myH, myW - 10, 5);
     
-    // Draw screen borders
-    context.strokeRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "red";
+    context.fillRect(myX + 5, myY, myW - 10, 5);
+    
+    context.fillStyle = "yellow";
+    context.fillRect(myX, myY + 5, 5, myH - 10);
+    
+    context.fillStyle = "purple";
+    context.fillRect(myX + myW, myY + 5, 5, myH - 10);
 }
 
-function keyup(key) {
-    // when SPACE pressed - pause/unpause game
-    if(key == 32) {
-        if(isUpdatePaused()) {
-            // Start update repeating
-            startUpdate(); 
-        } else {
-            // Stop update repeating
-            pauseUpdate();
-        }
+function keydown(key) {
+    if((key == 32 || key == 87 || key == 38) && isOnFloor) {
+        myDY = -5;
     }
 }
 
 function mouseup() {
-	console.log(mouseX, mouseY);
 }
